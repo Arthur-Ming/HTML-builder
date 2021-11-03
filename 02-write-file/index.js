e30f2f6668819295650f61abc4361051d185c56f
Artur Mingazhov <arthurming7@gmail.com>
2021-11-03 12:05:18 +0300

    feat: add solution for write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const process = require('process');
+const path = require('path')
+const readline = require('readline');
+
+const text = path.join(__dirname, 'text.txt')
+const stream = new fs.WriteStream(text, { encoding: 'utf-8' });
+
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout,
+   prompt: 'Enter your text: '
+});
+
+rl.prompt();
+
+rl.on('line', (input) => {
+   if (input === 'exit') {
+      rl.close();
+      process.exit()
+   }
+
+   stream.write(input + '\n')
+
+})
+rl.on('close', () => {
+   console.log('Goodbye!')
+});
