b550fb017bb35b16885c927acd543581a0368d6c
Artur Mingazhov <arthurming7@gmail.com>
2021-11-03 13:50:15 +0300

    feat: add solution for files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require('path')
+
+const dir = path.join(__dirname, "secret-folder");
+
+fs.readdir(dir, (err, files) => {
+   if (err) throw err;
+
+   for (const file of files) {
+
+      const filePath = path.join(dir, file);
+
+      fs.stat(filePath, (errStat, status) => {
+
+         if (errStat) throw errStat;
+
+         if (status.isFile()) {
+            const { name, ext } = path.parse(filePath)
+            console.log(`${name} - ${ext.slice(1)} - ${(status.size) / 1000}kb`)
+         }
+      });
+   }
+});
